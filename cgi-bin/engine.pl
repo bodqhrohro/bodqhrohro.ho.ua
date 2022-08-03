120d43d3a73fc2812ecddc7ba8247f2f84790cf1
Bohdan Horbeshko <bodqhrohro@gmail.com>
2022-08-03 22:10:59 +0300

    Initial commit

---
+++ cgi-bin/engine.pl
@@ -0,0 +1,120 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use v5.10;
+
+use CGI ':cgi', ':cgi-lib';
+use URI::Escape;
+use POSIX 'strftime';
+
+sub ReadTextFile {
+	my $TF;
+	open(TF, $_[0]);
+	my $content;
+	{
+		local $/;
+		$content = <TF>;
+	}
+	close(TF);
+	return $content;
+};
+
+sub ExecHTPL {
+	my $EPres = '';
+	state $FIN = 'FIN'; # TODO: make random
+	state $chomp = "\nchomp \$EPres;";
+	my $text_ = "\$EPres = <<$FIN;\n$_[0]\n$FIN$chomp";
+	$text_ =~ s/<\?=(.+?)\?>/\n$FIN$chomp\n\$EPres \.= \($1\) \. <<$FIN;\n/gms;
+	$text_ =~ s/<\?/\n$FIN$chomp\n/g;
+	$text_ =~ s/\?>/\n\$EPres \.= <<$FIN;\n/g;
+	eval($text_);
+	$EPres = "Error: $@" if $@;
+	return $EPres;
+};
+
+# UTILS 
+
+sub block {
+	my $name = shift @_;
+	return ExecHTPL(ReadTextFile($main::root_dir . '/_blocks/' . $name . '.htpl'), @_);
+};
+
+sub cookie_or_get {
+	my $name = $_[0];
+	my $use_cookies = cookie(-name=>'use_cookies');
+	return $use_cookies == 1 ? cookie(-name=>$name) : url_param($name);
+};
+
+sub loc {
+	return $_[0]->{common} // $_[0]->{$main::lang};
+};
+
+sub llink { # local link
+	my $link = shift @_;
+	return alink($main::root_dir_url . '/' . $link, @_);
+};
+
+sub alink { # absolute link
+	my %params = Vars();
+	if (defined($_[1])) {
+		my $arg_params = $_[1];
+		for my $key (keys %{$arg_params}) {
+			$params{$key} = $arg_params->{$key};
+		}
+	}
+	if (defined($_[2])) {
+		my @exclude = @{$_[2]};
+		for my $key (@exclude) {
+			delete $params{$key};
+		}
+	}
+	my $query_string = join('&', map { uri_escape($_) . '=' . uri_escape($params{$_}) } keys %params);
+	$query_string = '?' . $query_string if $query_string ne '';
+	return $_[0] . $query_string;
+};
+
+sub current_page {
+	my $path = path_info();
+	if (index($path, $main::root_dir_url) == 0) {
+		$path = substr($path, length($main::root_dir_url));
+	}
+	$path =~ s!^/!!;
+	return $path;
+}
+
+sub canonical_url {
+	return alink(url( -path_info=>1 ), {}, @main::known_cookie_keys);
+};
+
+# CONSTANTS
+
+{
+	no warnings 'once';
+	$main::fn = $ENV{'DOCUMENT_ROOT'} . $ENV{'PATH_INFO'};
+	$main::known_stable_keys = [ 'lang' ];
+	$main::known_cookie_keys = [ 'theme' ];
+	$main::languages = [ 'ua', 'en', 'ru' ];
+	$main::modification_time = (stat $main::fn)[9];
+}
+
+# MAIN
+
+$main::root_dir_url = '/new';
+$main::root_dir = $ENV{'DOCUMENT_ROOT'} . $main::root_dir_url;
+
+$main::lang = (url_param('lang') or '');
+if (! grep {$_ eq $main::lang} @{$main::languages}) {
+	$main::lang = 'ua';
+}
+$main::inject_headers = {};
+
+# execute the template first so sending headers like redirects is possible
+my $templ_ = ReadTextFile($main::fn);
+
+my $htpl = ExecHTPL($templ_);
+
+$main::inject_headers->{-type} = 'text/html';
+$main::inject_headers->{-charset} = 'utf-8';
+print header $main::inject_headers;
+
+print $htpl;
---
+++ htdocs/static/scripts/graphic.js
@@ -0,0 +1,134 @@
+﻿window.onload = function() {
+ var photothumb1 = document.getElementById('photothumb1');
+ if (photothumb1) {
+  photothumb1.onmouseover = function(e) {
+   var e = e || window.event;
+   thumbpopup('static/images/photo1.jpg', e.x, e.y);
+  };
+  photothumb1.onmouseout = delthumbpopup;
+ }
+
+ var body1 = document.getElementById('body1');
+ if (body1) {
+  var cursor = document.createElement('img');
+  cursor.setAttribute('src', 'data:image/gif;base64,R0lGODlhCAAQAIABAAD/AP///yH/C05FVFNDQVBFMi4wAwEAAAAh+QQJMgABACwAAAAACAAQAAACDIyPqcvtD6OcoNpaAAAh+QQFMgABACwAAAAACAAQAAACCoyPqcvtD6OcrAAAOw==');
+  cursor.setAttribute('id', 'cursor1');
+  body1.appendChild(cursor);
+
+  window.document.body.onkeypress = function(e) {
+    var e = e || window.event;
+    emulconsole1(e.keyCode);
+  };
+ }
+
+ var resizeHandler = function() {
+  var width = parseInt(document.documentElement.clientWidth);
+  var height = parseInt(document.documentElement.clientHeight);
+  var marginX = ((width - (((width - 20) / 10) |0) * 10) / 2) + 'px';
+  var marginY = ((height - (((height - 36) / 18) |0) * 18) / 2) + 'px';
+  document.body.style.marginLeft = marginX;
+  document.body.style.marginRight = marginX;
+  document.body.style.marginTop = marginY;
+  document.body.style.marginBottom = marginY;
+
+  var title = document.getElementById('title');
+  if (title) {
+   var rect = title.getBoundingClientRect();
+   if (rect) {
+    title.style.position = 'relative';
+    title.style.left = (-(rect.x - ((rect.x / 10) |0) * 10)) + 'px';
+   }
+  }
+ };
+ window.onresize = resizeHandler;
+ resizeHandler();
+};
+
+var timerID;
+function thumbpopup(path1, left, top) {
+ var i = 0;
+ var photo1;
+ var photosizeinc = function() {
+  i += 4;
+  photo1.style.width = i + 'px';
+  photo1.style.height = i + 'px';
+  if (i >= 128) {
+   clearInterval(timerID);
+   i -= 4;
+  }
+ };
+ var thumb = document.createElement('div');
+ thumb.id = 'thumbpopup';
+ thumb.style.position = 'absolute';
+ thumb.style.top = top + 18 + 'px';
+ thumb.style.left = left + 'px';
+ thumb.style.backgroundcolor = '#ffffff';
+ thumb.innerHTML = '<img id="photo1" src="'+path1+'" alt="thumb" onmouseout="delthumbpopup()" style="width: 0px; height: 0px;">';
+ document.body.appendChild(thumb);
+ photo1 = document.getElementById('photo1');
+ timerID = setInterval(photosizeinc, 1);
+};
+function delthumbpopup(){
+ thumb = document.getElementById('thumbpopup');
+ if (thumb) {
+  document.body.removeChild(thumb);
+ }
+};
+
+var lttrcount1 = 0;
+function emulconsole1(code1) {
+ var lttr = document.createElement('span');
+ lttr.setAttribute('id', 'lttr'+lttrcount1);
+ lttr.innerHTML = String.fromCharCode(code1);
+ if (code1==13) {
+  lttr.innerHTML='<br>';
+ };
+ cur1 = document.getElementById('cursor1');
+ var body1 = document.getElementById('body1');
+ body1.insertBefore(lttr, cur1);
+ lttrcount1++;
+};
+
+function safelink(link) {
+ var i, tmp1;
+ var link2='';
+ for (i=0; i<=link.length-1; i++) {
+  link2 += String.fromCharCode(link.charCodeAt(i) + 1);
+ };
+ window.location = link2;
+};
+function openiframe(path, width, height) {
+ var borderedWidth = width + 20;
+ var borderedHeight = height + 36;
+ var container = document.createElement('div');
+ container.setAttribute('id','playerlayer1');
+ container.style.width = borderedWidth+'px';
+ container.style.height = borderedHeight+'px';
+ container.style.top = ((document.body.clientHeight - borderedHeight) / 2 + parseInt(document.body.style.marginTop)) + 'px';
+ container.style.left = ((document.body.clientWidth - borderedWidth) / 2 + parseInt(document.body.style.marginLeft)) + 'px';
+ var close = document.createElement('span');
+ close.className = 'close-button';
+ close.innerHTML = 'X';
+ container.appendChild(close);
+ var iframe = document.createElement('iframe');
+ iframe.setAttribute('src', path);
+ iframe.setAttribute('allowtransparency', '');
+ iframe.setAttribute('frameborder', 0);
+ iframe.setAttribute('hspace', 0);
+ iframe.setAttribute('vspace', 0);
+ iframe.setAttribute('height', height + 'px');
+ iframe.setAttribute('width', width + 'px');
+ container.appendChild(iframe);
+ document.body.appendChild(container);
+ var shadow = document.createElement('div');
+ shadow.setAttribute('id', 'upperlayer1');
+ shadow.style.width = document.body.clientWidth + 'px';
+ shadow.style.height = document.body.clientHeight + 'px';
+ document.body.appendChild(shadow);
+
+ close.onclick = closeplayer;
+}
+function closeplayer(){
+ document.body.removeChild(document.getElementById('upperlayer1'));
+ document.body.removeChild(document.getElementById('playerlayer1'));
+}
